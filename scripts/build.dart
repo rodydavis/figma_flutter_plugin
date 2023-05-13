265781c74e8ac3e7b1ccf1d44a7f64b2264f9bf7
Rody Davis <rody.davis.jr@gmail.com>
2023-05-12 17:14:56 -0700

    icons fonts fix

--- scripts/build.dart
+++ scripts/build.dart
@@ -9,6 +9,7 @@ void main() async {
     '--pwa-strategy=none',
     '--web-renderer=html',
     '--base-href=/figma_flutter_plugin/',
+    '--dart2js-optimization=O1',
   ]);
   // <script src="flutter.js" defer></script>
   final outFile = File('build/web/index.html');
@@ -35,10 +36,12 @@ void main() async {
   }
   {
     final jsFile = File('build/web/main.dart.js');
-    final js = await jsFile.readAsString();
+    String js = await jsFile.readAsString();
     final sb = StringBuffer();
     sb.writeln('<script id="app">');
+    js = js.replaceAll('self.window.fetch', 'FETCH');
     sb.writeln(js);
+    sb.writeln(OVERRIDES);
     sb.writeln('</script>');
     html = html.replaceAll(
       '<script id="app" src="main.dart.js" defer></script>',
@@ -59,3 +62,49 @@ Future<void> run(String command, [List<String> args = const []]) async {
     exit(result.exitCode);
   }
 }
+
+const OVERRIDES = r'''
+// Fix for fetch
+function FETCH(url, options = {}) {
+  console.log('fetch', url, options);
+  return new Promise(function (resolve, reject) {
+    const id = Math.random().toString(36).substring(2, 15);
+    const message = {
+      pluginMessage: {
+        msg_type: 'fetch',
+        id: id,
+        url: url,
+        options: options,
+      },
+    };
+    window.parent.postMessage(message, '*');
+    window.addEventListener('message', function (event) {
+      const msg = event.data.pluginMessage;
+      if (msg.msg_type === 'fetch' && msg.id === id) {
+        console.log('fetch response', msg);
+        if (msg.error) {
+          reject(msg.error);
+        } else {
+          const raw = msg.result; // Array buffer
+          resolve(new Response(raw, {
+            status: msg.status,
+            statusText: msg.statusText,
+            headers: msg.headers,
+          }));
+        }
+      }
+    });
+  });
+};
+
+// Override window fetch
+window.fetch = FETCH;
+
+// Polyfill history.replaceState
+function replaceState(state, title, url) {
+  console.log('replaceState', state, title, url);
+  return undefined;
+}
+window.history = window.history || {};
+window.history.replaceState = replaceState;
+''';
--- web/code.js
+++ web/code.js
@@ -1,3 +1,41 @@
 /// <reference path="../third_party/figma/index.d.ts" />
 
-figma.showUI(__html__, { width: 300, height: 400 });
+figma.showUI(__html__, { width: 400, height: 500 });
+
+figma.ui.onmessage = async (msg) => {
+  if (msg.msg_type) {
+    switch (msg.msg_type) {
+      case "fetch":
+        let { url, options, id } = msg;
+        if (!url.startsWith("http")) {
+          url = `https://rodydavis.github.io/figma_flutter_plugin/${url}`;
+        }
+        try {
+          const res = await fetch(url, options);
+          const meta = Object.assign({}, res);
+          // Delete functions
+          for (const key in meta) {
+            if (typeof meta[key] === "function") {
+              delete meta[key];
+            }
+          }
+          meta.headers = meta.headersObject;
+          delete meta.headersObject;
+
+          const raw = await res.arrayBuffer();
+          const result = Object.assign(
+            { msg_type: "fetch", result: raw, id },
+            meta
+          );
+
+          console.log('fetch result', result);
+          figma.ui.postMessage(result);
+        } catch (error) {
+          figma.ui.postMessage({ msg_type: "fetch", error, id, headers });
+        }
+        break;
+      default:
+        break;
+    }
+  }
+};
